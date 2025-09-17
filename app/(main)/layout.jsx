fae26b2e6d2715f02b6ebbb0986e47cc264af044
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-09-17 15:58:19 +0600

    bug fixd

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -12,26 +11,3 @@ export default function MainLayout({children}){
         </>
     );
 }
-=======
-import Footer from "@/components/Footer";
-import { CartProvider } from "@/lib/cart";
-import Navbar from "@/components/Nav-bar";
-
-export const metadata = {
-  title: "জামদানি শাড়ি - ঐতিহ্যবাহী বাংলাদেশী হস্তশিল্প",
-  description:
-    "অনন্য কারুশিল্প এবং সাংস্কৃতিক ঐতিহ্য সমৃদ্ধ ঐতিহ্যবাহী বাংলাদেশী জামদানি শাড়ি অন্বেষণ করুন। খাঁটি জামদানি শাড়ির প্রিমিয়াম সংগ্রহ।",
-};
-
-export default function MainLayout({ children }) {
-  return (
-    <>
-      <CartProvider>
-        <Navbar />
-        <main className="flex-grow">{children}</main>
-        <Footer />
-      </CartProvider>
-    </>
-  );
-}
->>>>>>> d629957c9e76c53ff431c6e99d4e4a113b85e68d
